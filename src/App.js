fcc3b5267444fee5754cd529c047d5082f803c44
minnyww <minrrverse@gmail.com>
2021-06-21 15:33:50 +0700

    [init]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import liff from "@line/liff";
 
-function App() {
+export default function App() {
+  const [userProfile, setUserProfile] = useState();
+  const initialize = useCallback(async () => {
+    liff.init({ liffId: "1654439095-No9bR5K2" });
+    if (liff.isLoggedIn()) {
+      const user = await liff.getProfile();
+      setUserProfile(user);
+    } else if (!liff.isLoggedIn()) {
+      liff.login();
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("load", initialize());
+  }, [initialize]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users userProfile={userProfile} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users({ userProfile }) {
+  return <h2>Users : {JSON.stringify(userProfile)}</h2>;
+}
